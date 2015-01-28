5e7e526c5101a21d3f320d0921b0f22d7504452c
Sirface <kevino9339@gmail.com>
2015-01-28 14:33:27 +0100

    Added infinite loop

--- Test/Program.cs
+++ Test/Program.cs
@@ -13,6 +13,16 @@ namespace Test
         static void Main(string[] args)
         {
             CTimer timer = new CTimer(1000);
+
+            while (true)
+            {
+                if (timer.isDone())
+                {
+                    Console.WriteLine("Still going");
+                    timer.reset(1000);
+                }
+            }
+
             DateTime Today = DateTime.Today;
             Console.WriteLine("Hello World!");
             Console.WriteLine(Today.Date);
