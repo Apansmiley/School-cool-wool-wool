5972da1f2887672bdf46446ed9ef371b4c6fd02d
Sirface <kevino9339@gmail.com>
2015-01-28 14:22:14 +0100

    La till Timer klass

---
+++ Test/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Test
+{
+    class CTimer
+    {
+        Stopwatch watch;
+        long stopTime;
+        public CTimer(long ms)
+        {
+            watch = new Stopwatch();
+            watch.Start();
+            stopTime = watch.ElapsedMilliseconds + ms;
+        }
+
+        public bool isDone()
+        {
+            if (stopTime <= watch.ElapsedMilliseconds)
+                return true;
+            return false;
+        }
+    }
+}
