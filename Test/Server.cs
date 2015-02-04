2bd0fafe4149f530439dcdd63241a53192911795
Apansmiley <apansmiley@gmail.com>
2015-02-04 12:04:02 +0100

    yolo

--- Test/Server.cs
+++ Test/Server.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Net.Sockets;
 using System.Net;
 
@@ -10,8 +11,10 @@ namespace Test
 {
     class CServer
     {
+        TcpListener server;
         public void Start()
         {
+     
             IPHostEntry host;
             string LocalIp = "";
             host = Dns.GetHostEntry(Dns.GetHostName());
@@ -24,33 +27,51 @@ namespace Test
                 }
                 
             }
-            TcpListener server = new TcpListener(IPAddress.Parse(LocalIp), 80);
+             server = new TcpListener(IPAddress.Parse(LocalIp), 80);
 
             server.Start();
             Console.WriteLine("Server has started on: " + LocalIp +  "{0}Waiting for a connection...", Environment.NewLine);
+        } 
 
-            TcpClient client = server.AcceptTcpClient();
-
-            Console.WriteLine("A client connected.");
-
-            NetworkStream stream = client.GetStream();
-
-            //enter to an infinite cycle to be able to handle every change in stream
+         public void Clientconnected ()
+          {
             while (true)
             {
-                while (!stream.DataAvailable) ;
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connection accepted.");
+                NetworkStream stream = client.GetStream();
 
-                Byte[] bytes = new Byte[client.Available];
+                var childTcpclientThread = new Thread(() =>
+                {
+                    // while (!stream.DataAvailable) ;
+                    Byte[] bytes = new Byte[client.Available];
 
-                stream.Read(bytes, 0, bytes.Length);
-                string line = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(line);
+                    stream.Read(bytes, 0, bytes.Length);
+                    string line = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(line);
+                    
+                });
+                
+
+                                
                 Console.ForegroundColor = ConsoleColor.Blue;
                 string message = Console.ReadLine();
                 byte[] byteBuffer = Encoding.ASCII.GetBytes(message);
                 stream.Write(byteBuffer, 0, byteBuffer.Length);
+               
+                childTcpclientThread.Start();
             }
+          }
+
+         
         }
+        static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            Console.WriteLine("Exit");
+            
+
+        }
+       
     }
 }
