7ef8c888fbf27e20b61bc59eea4c858986dd9df4
Apansmiley <apansmiley@gmail.com>
2015-02-04 12:42:47 +0100

    Sexy server code

--- Test/Program.cs
+++ Test/Program.cs
@@ -31,7 +31,7 @@ namespace Test
                 break;
             }
 
-            Console.ReadKey();
+          
         }
     }    
 }
--- Test/Server.cs
+++ Test/Server.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Net.Sockets;
 using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Test
 {
     class CServer
     {
         TcpListener server;
+        Thread ClientConnection;
+        NetworkStream stream;
+        Thread ClientMessage;
+        TcpClient client;
+        Thread ClientRead;
         public void Start()
         {
-     
+
             IPHostEntry host;
             string LocalIp = "";
             host = Dns.GetHostEntry(Dns.GetHostName());
@@ -25,53 +30,64 @@ namespace Test
                     LocalIp = ip.ToString();
                     break;
                 }
-                
+
             }
-             server = new TcpListener(IPAddress.Parse(LocalIp), 80);
+            server = new TcpListener(IPAddress.Parse(LocalIp), 80);
 
             server.Start();
-            Console.WriteLine("Server has started on: " + LocalIp +  "{0}Waiting for a connection...", Environment.NewLine);
-        } 
+            Console.WriteLine("Server has started on: " + LocalIp + "{0}Waiting for a connection...", Environment.NewLine);
+            ClientConnection = new Thread(new ThreadStart(Clientconnected));
+            ClientConnection.Start();
+            ClientMessage = new Thread(new ThreadStart(Clientmessage));
+            ClientMessage.Start();
+            //ClientRead = new Thread(new ThreadStart(Getmessage));
+            //ClientRead.Start(stream);
 
-         public void Clientconnected ()
-          {
+
+
+        }
+
+        public void Clientconnected()
+        {
             while (true)
             {
-                TcpClient client = server.AcceptTcpClient();
+                client = server.AcceptTcpClient();
                 Console.WriteLine("Connection accepted.");
-                NetworkStream stream = client.GetStream();
+                stream = client.GetStream();
+                var threading = new Thread(() => Getmessage(stream));
+                threading.Start();
+            }
+        }
 
-                var childTcpclientThread = new Thread(() =>
+        public void Clientmessage()
+        {
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                string message = Console.ReadLine();
+                byte[] byteBuffer = Encoding.ASCII.GetBytes(message);
+                stream.Write(byteBuffer, 0, byteBuffer.Length);
+            }
+        }
+        public void Getmessage(NetworkStream stream)
+        {
+            while (true)
+            {
+                if (stream != null)
                 {
-                    // while (!stream.DataAvailable) ;
+                    while (!stream.DataAvailable) ;
                     Byte[] bytes = new Byte[client.Available];
 
                     stream.Read(bytes, 0, bytes.Length);
                     string line = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine(line);
-                    
-                });
-                
-
-                                
-                Console.ForegroundColor = ConsoleColor.Blue;
-                string message = Console.ReadLine();
-                byte[] byteBuffer = Encoding.ASCII.GetBytes(message);
-                stream.Write(byteBuffer, 0, byteBuffer.Length);
-               
-                childTcpclientThread.Start();
+                }
             }
-          }
-
-         
         }
-        static void CurrentDomain_ProcessExit(object sender, EventArgs e)
-        {
-            Console.WriteLine("Exit");
-            
-
-        }
-       
     }
+
+
 }
+
